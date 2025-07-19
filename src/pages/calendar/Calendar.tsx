fb95af1cd6b8e4343928da90f3a94f4c6e654c6b
woogamjaa <woogamjaa@gmail.com>
2025-07-19 15:42:23 +0900

    feat: 일정생성 모달 1.일정생성 모달창 초기 기본틀 생성 2.플러스 버튼 누를 시 나오게 설정

--- src/pages/calendar/Calendar.tsx
+++ src/pages/calendar/Calendar.tsx
@@ -1,27 +1,42 @@
-// pages/calendar/Calendar.tsx - 최소한의 코드
-import React from 'react';
+// pages/calendar/Calendar.tsx - 모달 상태 관리 추가
+import React, { useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../store';
 import CalendarBase from '../../components/ui/CalendarBase';
 import CalendarSearchModal from '../../components/ui/CalendarSearchModal';
+import CalendarScheduleAddModal from '../../components/ui/CalendarScheduleAddModal';
 import CalendarDayClickModal from '../../components/ui/CalendarDayClickModal';
 import { updateCurrentDate, selectEvents } from '../../store/slices/calendarSlice';
 
 const Calendar: React.FC = () => {
   const dispatch = useAppDispatch();
   const events = useAppSelector(selectEvents);
+  
+  // 모달 상태 관리
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
   // 날짜 변경 핸들러
   const handleDatesSet = (dateInfo: any) => {
     dispatch(updateCurrentDate({ start: dateInfo.start }));
   };
 
+  // 일정 추가 모달 열기
+  const handleOpenAddModal = () => {
+    setIsAddModalOpen(true);
+  };
+
   return (
     <div className="calendar-container">
-      {/* 검색 모달 - 자체 관리 */}
+      {/* 검색 모달 - 기존 그대로 */}
       <CalendarSearchModal showSearchButton={true} />
       
-      {/* 날짜 클릭 모달 - 자체 관리 */}
-      <CalendarDayClickModal />
+      {/* 일정 추가 모달 */}
+      <CalendarScheduleAddModal 
+        isOpen={isAddModalOpen}
+        onClose={() => setIsAddModalOpen(false)}
+      />
+      
+      {/* 날짜 클릭 모달 - onOpenAddModal 연결 */}
+      <CalendarDayClickModal onOpenAddModal={handleOpenAddModal} />
       
       {/* 캘린더 */}
       <CalendarBase 
