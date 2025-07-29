15989db060780a43e2d2b17533f1a4fd1a6289a3
my2min0 <ffrf1234tina@gmail.com>
2025-07-29 16:55:36 +0900

    feat: onClick 이벤트 추가

--- src/assets/icons/CheckIcon.tsx
+++ src/assets/icons/CheckIcon.tsx
@@ -5,13 +5,15 @@ interface CheckIconProps {
     height?: number | string;
     fill?: string;
     className?: string;
+    onClick?: (event: React.MouseEvent<SVGSVGElement>) => void;
 }
 
 const CheckIcon: React.FC<CheckIconProps> = ({
     width = 26,
     height = 18,
     fill = 'currentColor',
-    className
+    className,
+    onClick
 }) => {
     return (
         <svg 
@@ -21,6 +23,7 @@ const CheckIcon: React.FC<CheckIconProps> = ({
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className={ className }
+            onClick={ onClick }
         >
         <path d="M25 1L9 17L1 9" stroke={ fill } stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
