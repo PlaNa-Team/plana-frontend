9739a19854a6db29a00ee5854dc910191eb80924
my2min0 <ffrf1234tina@gmail.com>
2025-12-10 16:21:16 +0900

    design: 로그인, 비번 변경

--- src/pages/auth/PasswordSearchPage.tsx
+++ src/pages/auth/PasswordSearchPage.tsx
@@ -137,8 +137,7 @@ const PasswordSearchPage: React.FC = () => {
         {/* 이메일 입력 (인증 완료 시 숨김) */}
         {!isVerified && (
           <div className="form-group">
-            <label className="input-label">이메일*</label>
-            <div className={`input-wrapper ${errors.emailError ? 'error' : ''}`}>
+            <div className={`input-wrapper email-input ${errors.emailError ? 'error' : ''}`}>
               <svg className="input-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path
                   d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"
